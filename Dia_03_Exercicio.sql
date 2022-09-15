8c68e5464cc00eb3674a15323a154d600b6d5e10
maleto <hermano.neto@academico.ifpb.edu.br>
2022-09-14 23:52:02 -0300

    Correção exercício

--- Dia_03_Exercicio.sql
+++ Dia_03_Exercicio.sql
@@ -4,13 +4,25 @@ use ExercicioFabrica;
 create table Medico(
 	CRM int,
     cpf int unique,
-    sexo char(1),
+    sexo enum('M', 'F'),
     nome varchar(60),
     
     primary key (CRM)
 );
 
 alter table Medico add column especializacao varchar(30);
+
+create table Pasciente(
+	CPF int,
+    nome int,
+    idade tinyint(3),
+    sexo enum('M', 'F'),
+    CRM_Medico int,
+    /* Levando em consideração que o pasciente vai a apenas um medico,
+     ou seja, relacionamento de n-1 */
+    foreign key (CRM_Medico) references Medico(CRM),
+    primary key (CPF, CRM_Medico)
+);
 
 insert into Medico(CRM, cpf, sexo, nome, especializacao)
 value ('123', '1234561', 'M', 'Hermano Neto', 'Ortopedista');
@@ -21,4 +33,5 @@ value ('124', '1234562', 'F', 'Maria Eduarda', 'Cardiologista');
 insert into Medico(CRM, cpf, sexo, nome, especializacao)
 value (125, 1234563, 'F', 'Maria Clara', 'Cardiologista');
 
-select * from Medico;
+
+
