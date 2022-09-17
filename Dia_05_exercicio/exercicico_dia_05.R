9549d20c9aad5ee69f55c3643d4a109b157eaaf5
maleto <hermano.neto@academico.ifpb.edu.br>
2022-09-17 10:26:23 -0300

    Adicionando Exercicio dia 05

---
+++ Dia_05_exercicio/exercicico_dia_05.R
@@ -0,0 +1,12 @@
+setwd('D:/faculdade/Unipe/Fabrica De Software/bd_Fabrica/Fabrica-de-Sofware/Dia_05_exercicio')
+
+df = read.csv('new-used-cars-dataset.csv')
+df
+
+View(df)
+
+View(df["used.certified"])
+
+summary(df)
+
+df = rename()
